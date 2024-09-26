042383589de7001439e867c6e7d2b855a0fb9944
mmanicheng <101378595+mmanicheng@users.noreply.github.com>
2024-09-26 15:34:04 +0800

    revise the carousel

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,7 +1,7 @@
-const carousel = document.getElementById('carousel');
-    const originalContent = `<a href="g/ab_ppt.html" alt="Powerpoint"><img src="assets/img/cover/gCover5.webp" /></a><a href="g/ad.html" alt="Real Estate Banner Ads"><img src="assets/img/cover/gCover_ad.webp" /></a><a href="g/jw_mgz.html" alt="Company Brochure"><img src="assets/img/cover/gCover_mgz.webp" /></a><a href="g/jwcamp.html" alt="Event Campaign"><img src="assets/img/cover/gCover_campaign.webp" /></a><a href="g/kb.html" alt="Educational Banner Ads"><img src="assets/img/cover/gCover2.webp" /></a>`;
+const carousel = document.getElementById("carousel");
+const originalContent = `<a href="g/ab_ppt.html" alt="Powerpoint"><img src="assets/img/cover/gCover5.webp" /></a><a href="g/ad.html" alt="Real Estate Banner Ads"><img src="assets/img/cover/gCover_ad.webp" /></a><a href="g/jw_mgz.html" alt="Company Brochure"><img src="assets/img/cover/gCover_mgz.webp" /></a><a href="g/jwcamp.html" alt="Event Campaign"><img src="assets/img/cover/gCover_campaign.webp" /></a><a href="g/kb.html" alt="Educational Banner Ads"><img src="assets/img/cover/gCover2.webp" /></a>`;
 
-    function addContent() {
+function addContent() {
       let contentToAdd = originalContent;
       carousel.innerHTML += contentToAdd;
       if (carousel.scrollWidth > window.innerWidth * 2) {
